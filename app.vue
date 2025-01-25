0537e36e7abc9cf59a41a6ada9c337fb468b7f68
onmax <maximogarciamtnez@gmail.com>
2025-01-25 12:01:47 +0100

    chore: first commit

---
+++ app.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { Sonner } from '@/components/ui/sonner'
+import { ConfigProvider } from 'radix-vue'
+
+const colorMode = useColorMode()
+
+const color = computed(() => colorMode.value === 'dark' ? '#09090b' : '#ffffff')
+
+useHead({
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { key: 'theme-color', name: 'theme-color', content: color },
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' },
+  ],
+  htmlAttrs: {
+    lang: 'en',
+  },
+})
+
+const { theme, radius } = useCustomize()
+
+useServerHead({
+  bodyAttrs: {
+    class: `theme-${theme.value}`,
+    style: `--radius: ${radius.value}rem;`,
+  },
+})
+
+const title = 'Nuxt Shadcn UI - Dashboard Template'
+const description = 'This dashboard, built with Nuxt, Shadcn UI, and UnoCSS. It includes a dark mode toggle and is optimized for performance and data efficiency.'
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
+  ogUrl: 'https://dashboard.dianprata.com',
+  ogImage: 'https://nuxt-shadcn-dashboard.vercel.app/social-card.png',
+  twitterTitle: title,
+  twitterDescription: description,
+  twitterImage: 'https://nuxt-shadcn-dashboard.vercel.app/social-card.png',
+  twitterCard: 'summary_large_image',
+})
+
+const router = useRouter()
+
+defineShortcuts({
+  'G-H': () => router.push('/'),
+  'G-E': () => router.push('/email'),
+})
+
+const useIdFunction = () => useId()
+</script>
+
+<template>
+  <ConfigProvider :use-id="useIdFunction">
+    <div vaul-drawer-wrapper>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </div>
+
+    <Toaster />
+    <Sonner class="pointer-events-auto" />
+  </ConfigProvider>
+</template>
